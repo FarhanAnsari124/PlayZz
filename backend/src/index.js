924372bd6277e5af10e358ceb661a270b00f8cde
Farhan Ansari <farhan.knp121@gmail.com>
2026-05-26 21:41:04 +0530

    utility for error and response

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,22 @@
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
-import express from 'express';
-import connectDB from './db/index.js';
+import express from "express";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
+
 connectDB()
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+  });
 
 // ( async ()=>{
 //     try {
